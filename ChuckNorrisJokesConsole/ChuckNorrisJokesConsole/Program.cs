39ca58d716836f1c165a50dd73a57d64491a5da2
AndileShezi <ag.shezi@gmail.com>
2019-10-02 15:11:51 +0200

    Intial Chuck Norris Commit

---
+++ ChuckNorrisJokesConsole/ChuckNorrisJokesConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using ChuckNorrisJokesLibrary;
+using System;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisJokesConsole
+{
+    class Program
+    {
+        
+        static async Task Main(string[] args)
+        {
+            JokeGenerator jokeGenerator = new JokeGenerator();
+            string randomJoke = await jokeGenerator.GetRandomJoke();
+
+            Console.WriteLine(randomJoke);
+        }
+    }
+}
---
+++ ChuckNorrisJokesConsole/ChuckNorrisJokesLibrary/JokeGenerator.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisJokesLibrary
+{
+    public class JokeGenerator
+    {
+        public async Task<string> GetRandomJoke()
+        {
+            var client = new HttpClient();
+
+            string randomJokeString = await client.GetStringAsync("https://api.chucknorris.io/jokes/random");
+
+            var joke = JsonConvert.DeserializeObject<Joke>(randomJokeString);
+
+            return joke.value;
+        }
+    }
+}
